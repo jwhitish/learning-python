8ac2f7377c6ab0be413e3fc9292e27f9c8eeaa9b
Jacob Whitish <jacobwhitish@gmail.com>
2017-08-22 20:30:59 -0700

    Add files via upload
    Automate the boring stuff exercise

---
+++ flipXY.py
@@ -0,0 +1,16 @@
+#in effect transposes the x/y coords to create a new image
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for j in range(len(grid[0])):
+    for i in range(len(grid)):
+        print(grid[i][j]),
+    print('')
